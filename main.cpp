6ddc0a200736b043e1f01239b0a58b5b41031d6a
JuanHirschmann <juanhirschmann@gmail.com>
2022-09-10 18:32:32 +0000

    Traslado archivos a su propio repo

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+int main()
+/**
+ * @brief Imprime continuamente "Hola" en el puerto serie.
+ */
+{
+    while (true) {
+        printf("Hola");
+    }
+}
